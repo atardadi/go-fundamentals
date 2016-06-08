ce8406535b1f7e9224908e644b1dd8f81dc06341
Dadi Atar <atardadi@gmail.com>
2016-06-08 09:27:55 +0300

    for loop

--- src/main.go
+++ src/main.go
@@ -3,19 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var plantCapacities []float64
-
-	plantCapacities = []float64{30, 30, 30, 60, 60, 100}
-
-	var capacity = plantCapacities[0] +
-		plantCapacities[1] + plantCapacities[2] +
-		plantCapacities[3] + plantCapacities[4] +
-		plantCapacities[5]
-
 	var gridLoad = 75.
+	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
+	activePlants := []int{0, 1}
 
-	utilization := gridLoad / capacity
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Print("Choose...")
 
-	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
-	fmt.Printf("%-20s%.0f\n", "Utilization:", utilization*100)
+	var option string
+
+	fmt.Scanln(&option)
+
+	switch option {
+	case "1":
+		for idx, cap := range plantCapacities {
+			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
+		}
+	case "2":
+		capacity := 0.
+
+		for _, plantID := range activePlants {
+			capacity += plantCapacities[plantID]
+		}
+		utilization := gridLoad / capacity
+
+		fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
+		fmt.Printf("%-20s%.1f%%\n", "Utilization:", utilization*100)
+	}
+
 }
